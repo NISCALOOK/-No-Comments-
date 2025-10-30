523dcb11ff7dd5ca071dfa1220a70a03d40dc32a
NigrumH0do <109390550+NigrumH0do@users.noreply.github.com>
2025-10-30 00:26:56 -0500

    Se agregaron diferentes tipos de respuesta para el Auth (Casos de clave incorrecta, correo el uso, etc)

---
+++ Proyecto/backend/src/main/java/com/classmateai/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.classmateai.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT) // <-- HTTP 409
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidCredentials(InvalidCredentialsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED) // <-- HTTP 401
+                .body(Map.of("error", ex.getMessage()));
+    }
+}
